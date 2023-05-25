35551bc5d28dde6a51ae0fb9f4384a7c348eab70
riegerjulio <riegerproducoes@gmail.com>
2023-05-25 12:11:44 -0300

    initial configs and header

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import * as React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+
+export default function MyApp() {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+}
